ba1067e0d28d16a7c25668238244d581ce35afcd
Duc Vu <de.yd315@gmail.com>
2019-04-10 18:14:25 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import { CharacterListView } from "./views";
 import "./styles/App.css";
-
+// 
 class App extends Component {
   render() {
     return <CharacterListView />;
